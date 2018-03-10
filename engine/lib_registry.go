2f26925341ac342f506233c9debfb391418f9fda
Alex Levinson <alexl@uber.com>
2018-03-09 22:39:15 -0800

    fixing some cross platform bugs

--- engine/lib_registry.go
+++ engine/lib_registry.go
@@ -4,14 +4,341 @@ package engine
 
 import "errors"
 
-func CreateRegKeyAndValue(regHive string, keyPath string, keyObject string, keyValue interface{}) error {
-	return errors.New("not implemented for this platform")
+// AddRegKeyString - Add a string registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyString(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value (string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyString(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyString(registryString string, path string, name string, value string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
 }
 
-func DeleteRegKeysValue(regHive string, keypath string, keyobject string) error {
-	return errors.New("not implemented for this platform")
+// AddRegKeyExpandedString - Add an expanded string registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyExpandedString(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value (string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyExpandedString(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyExpandedString(registryString string, path string, name string, value string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
 }
 
-func QueryRegKeyString(regHive string, keypath string, keyobject string) (string, error) {
-	return "error", errors.New("not implemented for this platform")
+// AddRegKeyBinary - Add a binary registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyBinary(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value ([]byte)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyBinary(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyBinary(registryString string, path string, name string, value []byte) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// AddRegKeyDWORD - Add a DWORD registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyDWORD(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value (uint32)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyDWORD(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyDWORD(registryString string, path string, name string, value uint32) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// AddRegKeyQWORD - Add a QWORD registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyQWORD(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value (uint64)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyQWORD(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyQWORD(registryString string, path string, name string, value uint64) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// AddRegKeyStrings - Add a registry key of type string(s)
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  AddRegKeyStrings(registryString, path, name, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * name (string)
+//  * value ([]string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = AddRegKeyStrings(registryString, path, name, value);
+//  // obj.runtimeError
+//
+func (e *Engine) AddRegKeyStrings(registryString string, path string, name string, value []string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// DelRegKey - Delete a registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  DelRegKey(registryString, path)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = DelRegKey(registryString, path);
+//  // obj.runtimeError
+//
+func (e *Engine) DelRegKey(registryString string, path string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// DelRegKeyValue - Delete a registry key value
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  DelRegKeyValue(registryString, path, value)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//  * value (string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = DelRegKeyValue(registryString, path, value);
+//  // obj.runtimeError
+//
+func (e *Engine) DelRegKeyValue(registryString string, path string, valueName string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+
+// QueryRegKey - Retrive a registry key
+//
+// Package
+//
+// os
+//
+// Author
+//
+// - Vyrus
+//
+// Javascript
+//
+// Here is the Javascript method signature:
+//  QueryRegKey(registryString, path)
+//
+// Arguments
+//
+// Here is a list of the arguments for the Javascript function:
+//  * registryString (string)
+//  * path (string)
+//
+// Returns
+//
+// Here is a list of fields in the return object:
+//  * obj.keyObj (RegistryRetValue)
+//  * obj.runtimeError (error)
+//
+// Example
+//
+// Here is an example of how to use this function in gscript:
+//  var obj = QueryRegKey(registryString, path);
+//  // obj.keyObj
+//  // obj.runtimeError
+//
+func (e *Engine) QueryRegKey(registryString string, path string) (RegistryRetValue, error) {
+	return RegistryRetValue{}, errors.New("this function is unimplemented on non windows platforms")
 }
--- engine/lib_registry_windows.go
+++ engine/lib_registry_windows.go
@@ -4,81 +4,171 @@ package engine
 
 import (
 	"errors"
-	"fmt"
-
-	reg "golang.org/x/sys/windows/registry"
 )
 
-// CreateRegKeyAndValue creates a new regestry key in a dynamic hive, dynamic path, dynamic object, and infers the type as either string or uint32 and creats the correct key type accordingly
-func CreateRegKeyAndValue(regHive string, keyPath string, keyObject string, keyValue interface{}) error {
-	var hive reg.Key
-	if regHive == "LM" {
-		hive = reg.LOCAL_MACHINE
-	} else if regHive == "CU" {
-		hive = reg.CURRENT_USER
-	} else {
-		return errors.New("invalid reg hive provided")
+var (
+	regKeys = map[string]registry.Key{
+		"CLASSES_ROOT", registry.CLASSES_ROOT,
+		"CURRENT_USER", registry.CURRENT_USER,
+		"LOCAL_MACHINE", registry.LOCAL_MACHINE,
+		"USERS", registry.USERS,
+		"CURRENT_CONFIG", registry.CURRENT_CONFIG,
+		"PERFORMANCE_DATA", registry.PERFORMANCE_DATA,
 	}
-	// Create our key or see if it exists
-	k, _, err := reg.CreateKey(hive, keyPath, reg.ALL_ACCESS)
-	if err != nil {
-		return errors.New("create key error")
+)
+
+func lookUpKey(keyString string) (registry.Key, error) {
+	key, ok := regKeys[keyString]
+	if !ok {
+		return nil, errors.New("Registry key " + keyString + " not found")
 	}
-	// regardless if its new or created it we set it to our value
-	defer k.Close()
-	// switch on keyValue type to create different key type values
-	switch v := keyValue.(type) {
-	case string:
-		keyValueSZ := keyValue.(string)
-		k.SetStringValue(keyObject, keyValueSZ)
-	case uint32:
-		keyValueUI32 := keyValue.(uint32)
-		k.SetDWordValue(keyObject, keyValueUI32)
-	default:
-		vstring := fmt.Sprintf("Info: %v", v)
-		return errors.New("add value error, Value " + vstring)
-	}
-	return nil
+	return key
 }
 
-// DeleteRegKeysValue deletes a dynamic keyobject in a dynamic hive and path
-func DeleteRegKeysValue(regHive string, keypath string, keyobject string) error {
-	var hive reg.Key
-	if regHive == "LM" {
-		hive = reg.LOCAL_MACHINE
-	} else if regHive == "CU" {
-		hive = reg.CURRENT_USER
-	} else {
-		return errors.New("invalid reg hive provided")
-	}
-	k, err := reg.OpenKey(hive, keypath, reg.ALL_ACCESS)
+func (e *Engine) AddRegKeyString(registryString string, path string, name string, value string) error {
+	regKey, err := lookUpKey(registryString)
 	if err != nil {
-		//log.Fatal(err)
-		return errors.New("couldn't open key")
+		return err
 	}
-	defer k.Close()
-	k.DeleteValue(keyobject)
-	return nil
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetStringValue(name, value)
 }
 
-// QueryRegKeyString reads and returns a registry key for windows
-func QueryRegKeyString(regHive string, keypath string, keyobject string) (string, error) {
-	var hive reg.Key
-	if regHive == "LM" {
-		hive = reg.LOCAL_MACHINE
-	} else if regHive == "CU" {
-		hive = reg.CURRENT_USER
-	} else {
-		return "error", errors.New("invalid reg hive provided")
-	}
-	k, err := reg.OpenKey(hive, keypath, reg.QUERY_VALUE)
+func (e *Engine) AddRegKeyExpandedString(registryString string, path string, name string, value string) error {
+	regKey, err := lookUpKey(registryString)
 	if err != nil {
-		return "error", errors.New("invalid reg path")
+		return err
 	}
-	defer k.Close()
-	s, _, err := k.GetStringValue(keyobject)
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
 	if err != nil {
-		return "error", errors.New("invalid key to query for")
+		return err
 	}
-	return s, nil
+	defer openRegKey.Close()
+	return openRegKey.SetExpandStringValue(name, value)
+}
+
+func (e *Engine) AddRegKeyBinary(registryString string, path string, name string, value []byte) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetBinaryValu(name, value)
+}
+
+func (e *Engine) AddRegKeyDWORD(registryString string, path string, name string, value uint32) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetDWordValue(name, value)
+}
+
+func (e *Engine) AddRegKeyQWORD(registryString string, path string, name string, value uint64) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetQWordValue(name, value)
+}
+
+func (e *Engine) AddRegKeyStrings(registryString string, path string, name string, value []string) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetStringsValue(name, value)
+}
+
+func (e *Engine) DelRegKey(registryString string, path string) error {
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return err
+	}
+	return registry.DeleteKey(regKey, path)
+}
+
+func (e *Engine) DelRegKeyValue(registryString string, path string, valueName string) error {
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return err
+	}
+	regKey.DeleteValue(name)
+	return registry.DeleteKey(reg, path)
+}
+
+func (e *Engine) QueryRegKey(registryString string, path string) (RegistryRetValue, error) {
+	retVal := new(RegistryRetValue)
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return retVal, err
+	}
+	openRegKey, err := registry.OpenKey(regKey, path, registry.QUERY_VALUE)
+	if err != nil {
+		return retVal, err
+	}
+	_, valType, err := openRegKey.GetValue(path, nil)
+	if err != nil {
+		return retVal, err
+	}
+	switch valType {
+	case registry.EXPAND_SZ:
+		value, _, err := openRegKey.GetStringsValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "StringArray"
+		retVal.StringArrayVal = value
+	case registry.SZ:
+		value, _, err := openRegKey.GetStringValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "String"
+		retVal.StringVal = value
+	case registry.BINARY:
+		value, _, err := openRegKey.GetBinaryValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "ByteArray"
+		retVal.ByteArrayVal = value
+	case registry.DWORD:
+		value, _, err := openRegKey.GetIntegerValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "Uint"
+		retVal.IntVal = uint32(value)
+	case registry.QWORD:
+		value, _, err := openRegKey.GetIntegerValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "Uint64"
+		retVal.LongVal = value
+	}
 }
