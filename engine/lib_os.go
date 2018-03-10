5573c513ef6137ec0a0c9338df4c1b4e5eef5675
vyrus <vyrus001@gmail.com>
2018-03-09 21:58:28 -0800

    added lib_os and lib_os_windows {hopefully working}

--- engine/lib_os.go
+++ engine/lib_os.go
@@ -1,422 +1,174 @@
 package engine
 
-// func LocalSystemInfo() ([]string, error) {
-// 	var InfoDump []string
-// 	gi := goInfo.GetInfo()
-// 	InfoDump = append(InfoDump, fmt.Sprintf("GoOS: %s", gi.GoOS))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("Kernel: %s", gi.Kernel))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("Core: %s", gi.Core))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("Platform: %s", gi.Platform))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("OS: %s", gi.OS))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("Hostname: %s", gi.Hostname))
-// 	InfoDump = append(InfoDump, fmt.Sprintf("CPUs: %v", gi.CPUs))
-// 	if InfoDump != nil {
-// 		return InfoDump, nil
-// 	}
-// 	return nil, errors.New("Failed to retrieve local system information")
-// }
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
 
-// func GetHostname() (string, error) {
-// 	gi := goInfo.GetInfo()
-// 	hostname := gi.Hostname
-// 	if hostname != "" {
-// 		return hostname, nil
-// 	}
-// 	return "", errors.New("Failed to retrieve local hostname")
-// }
+	"github.com/gen0cide/service-go"
+	"github.com/matishsiao/goInfo"
+	"github.com/mitchellh/go-ps"
+	"golang.org/x/sys/windows/registry"
+)
 
-// func ProcExists2(pidBoi int) bool {
-// 	process, err := ps.FindProcess(pidBoi)
-// 	if err == nil && process == nil {
-// 		return false
-// 	} else {
-// 		return true
-// 	}
-// }
+func (e *Engine) FindProcByName(procName string) (int error) {
+	// * FIXME: this function currently matches against the name of the executible which is NOT technically the proccess name
+	for _, proc := range ps.Processes() {
+		if procName == proc.Executable() {
+			return proc.Pid(), nil
+		}
+	}
+	return -1, errors.New("processes name not found")
+}
 
-// func FindProcessPid(key string) (int, error) {
-// 	pid := 0
-// 	err := errors.New("Not found")
-// 	ps, _ := ps.Processes()
-// 	for i, _ := range ps {
-// 		if ps[i].Executable() == key {
-// 			pid = ps[i].Pid()
-// 			err = nil
-// 			break
-// 		}
-// 	}
-// 	return pid, err
-// }
+func (e *Engine) InstallSystemService(path, name, displayName, description string) error {
+	c := &services.Config{
+		Path:        path,
+		Name:        name,
+		DisplayName: displayName,
+		Description: description,
+	}
 
-// func (e *Engine) InstallSystemService(path, name, displayName, description string) error {
-// 	c := &services.Config{
-// 		Path:        path,
-// 		Name:        name,
-// 		DisplayName: displayName,
-// 		Description: description,
-// 	}
+	s, err := services.NewServiceConfig(c)
+	if err != nil {
+		return err
+	}
 
-// 	s, err := services.NewServiceConfig(c)
-// 	if err != nil {
-// 		return err
-// 	}
+	err = s.Install()
+	if err != nil {
+		return err
+	}
 
-// 	err = s.Install()
-// 	if err != nil {
-// 		return err
-// 	}
+	return nil
+}
 
-// 	return nil
-// }
+func (e *Engine) StartServiceByName(name string) error {
+	c := &services.Config{
+		Name: name,
+	}
 
-// func (e *Engine) StartServiceByName(name string) error {
-// 	c := &services.Config{
-// 		Name: name,
-// 	}
+	s, err := services.NewServiceConfig(c)
+	if err != nil {
+		return err
+	}
 
-// 	s, err := services.NewServiceConfig(c)
-// 	if err != nil {
-// 		return err
-// 	}
+	err = s.Start()
+	if err != nil {
+		return err
+	}
 
-// 	err = s.Start()
-// 	if err != nil {
-// 		return err
-// 	}
+	return nil
+}
 
-// 	return nil
-// }
+func (e *Engine) StopServiceByName(name string) error {
+	c := &services.Config{
+		Name: name,
+	}
 
-// func (e *Engine) StopServiceByName(name string) error {
-// 	c := &services.Config{
-// 		Name: name,
-// 	}
+	s, err := services.NewServiceConfig(c)
+	if err != nil {
+		return err
+	}
 
-// 	s, err := services.NewServiceConfig(c)
-// 	if err != nil {
-// 		return err
-// 	}
+	err = s.Stop()
+	if err != nil {
+		return err
+	}
 
-// 	err = s.Stop()
-// 	if err != nil {
-// 		return err
-// 	}
+	return nil
+}
 
-// 	return nil
-// }
+func (e *Engine) RemoveServiceByName(name string) error {
+	c := &services.Config{
+		Name: name,
+	}
 
-// func (e *Engine) RemoveServiceByName(name string) error {
-// 	c := &services.Config{
-// 		Name: name,
-// 	}
+	s, err := services.NewServiceConfig(c)
+	if err != nil {
+		return err
+	}
 
-// 	s, err := services.NewServiceConfig(c)
-// 	if err != nil {
-// 		return err
-// 	}
+	err = s.Remove()
+	if err != nil {
+		return err
+	}
 
-// 	err = s.Remove()
-// 	if err != nil {
-// 		return err
-// 	}
+	return nil
+}
 
-// 	return nil
-// }
+func (e *Engine) Signal(proc int, sig int) error {
+	foundProc, err := os.FindProcess(proc)
+	if err != nil {
+		return err
+	}
+	return foundProc.Signal(sig)
+}
 
-// func (e *Engine) VMSignal(call otto.FunctionCall) otto.Value {
-// 	e.Logger.WithField("trace", "true").Errorf("Function Not Implemented")
-// 	return otto.FalseValue()
-// }
+//func (e *Engine) LoggedInUsers() ([]string, error) {}
 
-// func (e *Engine) VMLoggedInUsers(call otto.FunctionCall) otto.Value {
-// 	e.Logger.WithField("trace", "true").Errorf("Function Not Implemented")
-// 	return otto.FalseValue()
-// }
+func (e *Engine) RunningProcs() []int {
+	var procs []int
+	for _, proc := range ps.Processes() {
+		procs = append(procs, proc.Pid())
+	}
+	return procs
+}
 
-// func (e *Engine) VMUsersRunningProcs(call otto.FunctionCall) otto.Value {
-// 	e.Logger.WithField("trace", "true").Errorf("Function Not Implemented")
-// 	return otto.FalseValue()
-// }
+func (e *Engine) GetProcName(pid int) (string, error) {
+	// * FIXME: this function currently returns the name of the executible which is NOT technically the proccess name
+	proc, err := ps.FindProcess(pid)
+	if err != nil {
+		return nil, err
+	}
+	return proc.Executable().nil
+}
 
-// func (e *Engine) VMMemStats(call otto.FunctionCall) otto.Value {
-// 	e.Logger.WithField("trace", "true").Errorf("Function Not Implemented")
-// 	return otto.FalseValue()
-// }
+//func (e *Engine) UsersRunningProcs() ([]string, error) {}
 
-// func (e *Engine) VMCPUStats(call otto.FunctionCall) otto.Value {
-// 	cpuInfo, err := LocalSystemInfo()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("OS Error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	cpuStats := strings.Join(cpuInfo, "\n")
-// 	vmResponse, err := otto.ToValue(cpuStats)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Return value casting error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return vmResponse
-// }
+func (e *Engine) EnvVars() map[string]string {
+	vars := make(map[string]string)
+	for _, eVar := range os.Environ() {
+		eVarSegments := strings.Split(eVar, "=")
+		if len(eVarSegments) > 1 {
+			vars[eVarSegments[0]] = eVarSegments[1]
+		}
+	}
+	return vars
+}
 
-// func (e *Engine) VMEnvVars(call otto.FunctionCall) otto.Value {
-// 	rezultz := map[string]string{}
-// 	for _, v := range os.Environ() {
-// 		pair := strings.Split(v, "=")
-// 		rezultz[pair[0]] = pair[1]
-// 	}
-// 	vmResponse, err := e.VM.ToValue(rezultz)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Return value casting error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return vmResponse
-// }
+func (e *Engine) GetEnvVar(eVar string) string {
+	return os.Getenv(eVar)
+}
 
-// func (e *Engine) VMGetEnv(call otto.FunctionCall) otto.Value {
-// 	envVar := call.Argument(0)
-// 	envVarAsString, err := envVar.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	finalVal := os.Getenv(envVarAsString.(string))
-// 	vmResponse, err := e.VM.ToValue(finalVal)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Return value casting error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return vmResponse
-// }
+// func (e *Engine) LocalUserExists(user string) bool
 
-// func (e *Engine) VMAddRegKey(call otto.FunctionCall) otto.Value {
-// 	regHive := call.Argument(0)
-// 	keyPath := call.Argument(1)
-// 	keyObject := call.Argument(2)
-// 	keyValue := call.Argument(3)
-// 	keyValueInterface, err := keyValue.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	regHiveAsString, err := regHive.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyPathAsString, err := keyPath.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyObjectAsString, err := keyObject.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	err = CreateRegKeyAndValue(regHiveAsString.(string), keyPathAsString.(string), keyObjectAsString.(string), keyValueInterface)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Registry error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return otto.TrueValue()
-// }
-
-// func (e *Engine) VMDelRegKey(call otto.FunctionCall) otto.Value {
-// 	regHive := call.Argument(0)
-// 	keyPath := call.Argument(1)
-// 	keyObject := call.Argument(2)
-// 	regHiveAsString, err := regHive.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyPathAsString, err := keyPath.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyObjectAsString, err := keyObject.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	err = DeleteRegKeysValue(regHiveAsString.(string), keyPathAsString.(string), keyObjectAsString.(string))
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Registry error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return otto.TrueValue()
-// }
-
-// func (e *Engine) VMQueryRegKey(call otto.FunctionCall) otto.Value {
-// 	regHive := call.Argument(0)
-// 	keyPath := call.Argument(1)
-// 	keyObject := call.Argument(2)
-// 	regHiveAsString, err := regHive.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyPathAsString, err := keyPath.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	keyObjectAsString, err := keyObject.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	resultStringValue, err := QueryRegKeyString(regHiveAsString.(string), keyPathAsString.(string), keyObjectAsString.(string))
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Registry error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	vmResponse, err := e.VM.ToValue(resultStringValue)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Return value casting error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	return vmResponse
-// }
-
-// func (e *Engine) VMLocalUserExists(call otto.FunctionCall) otto.Value {
-// 	filePathString := "/etc/passwd"
-// 	search := call.Argument(0)
-// 	searchString, err := search.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	fileData, err := LocalFileRead(filePathString)
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("File error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	fileStrings := string(fileData)
-// 	if strings.Contains(fileStrings, searchString.(string)) {
-// 		return otto.TrueValue()
-// 	}
-// 	return otto.FalseValue()
-// }
-
-// func (e *Engine) VMProcExistsWithName(call otto.FunctionCall) otto.Value {
-// 	searchProc := call.Argument(0)
-// 	searchProcString, err := searchProc.Export()
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("Parameter parsing error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	ProcPID, err := FindProcessPid(searchProcString.(string))
-// 	if err != nil {
-// 		e.Logger.WithField("trace", "true").Errorf("OS error: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-// 	ProcExistsResult := ProcExists2(ProcPID)
-// 	if ProcExistsResult {
-// 		return otto.TrueValue()
-// 	}
-// 	return otto.FalseValue()
-// }
-
-// func (e *Engine) VMInstallSystemService(call otto.FunctionCall) otto.Value {
-// 	if len(call.ArgumentList) != 4 {
-// 		e.Logger.Errorf("Not enough arguments provided.")
-// 		return otto.FalseValue()
-// 	}
-
-// 	path, err := call.Argument(0).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting path to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	name, err := call.Argument(1).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting name to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	displayName, err := call.Argument(2).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting displayName to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	description, err := call.Argument(3).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting description to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	err = e.InstallSystemService(path, name, displayName, description)
-// 	if err != nil {
-// 		e.Logger.Errorf("Error installing system service: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	return otto.TrueValue()
-// }
-
-// func (e *Engine) VMStartServiceByName(call otto.FunctionCall) otto.Value {
-// 	if len(call.ArgumentList) != 1 {
-// 		e.Logger.Errorf("Not enough arguments provided.")
-// 		return otto.FalseValue()
-// 	}
-
-// 	name, err := call.Argument(0).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting name to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	err = e.StartServiceByName(name)
-// 	if err != nil {
-// 		e.Logger.Errorf("Error starting system service: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	return otto.TrueValue()
-// }
-
-// func (e *Engine) VMStopServiceByName(call otto.FunctionCall) otto.Value {
-// 	if len(call.ArgumentList) != 1 {
-// 		e.Logger.Errorf("Not enough arguments provided.")
-// 		return otto.FalseValue()
-// 	}
-
-// 	name, err := call.Argument(0).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting name to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	err = e.StopServiceByName(name)
-// 	if err != nil {
-// 		e.Logger.Errorf("Error starting system service: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	return otto.TrueValue()
-// }
-
-// func (e *Engine) VMRemoveServiceByName(call otto.FunctionCall) otto.Value {
-// 	if len(call.ArgumentList) != 1 {
-// 		e.Logger.Errorf("Not enough arguments provided.")
-// 		return otto.FalseValue()
-// 	}
-
-// 	name, err := call.Argument(0).ToString()
-// 	if err != nil {
-// 		e.Logger.Errorf("Error converting name to string: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	err = e.RemoveServiceByName(name)
-// 	if err != nil {
-// 		e.Logger.Errorf("Error starting system service: %s", err.Error())
-// 		return otto.FalseValue()
-// 	}
-
-// 	return otto.TrueValue()
-// }
+// stubs for windows only funcs
+func (e *Engine) AddRegKeyString(registryString string, path string, name string, value string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) AddRegKeyExpandedString(registryString string, path string, name string, value string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) AddRegKeyBinary(registryString string, path string, name string, value []byte) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) AddRegKeyDWORD(registryString string, path string, name string, value uint32) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) AddRegKeyQWORD(registryString string, path string, name string, value uint64) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) AddRegKeyStrings(registryString string, path string, name string, value []string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) DelRegKey(registryString string, path string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) DelRegKeyValue(registryString string, path string, valueName string) error {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
+func (e *Engine) QueryRegKey(key string) (RegistryRetValue, error) {
+	return errors.New("this function is unimplemented on non windows platforms")
+}
---
+++ engine/lib_os_windows.go
@@ -0,0 +1,183 @@
+package engine
+
+import (
+	"errors"
+
+	"golang.org/x/sys/windows/registry"
+)
+
+var (
+	regKeys = map[string]registry.Key{
+		"CLASSES_ROOT", registry.CLASSES_ROOT,
+		"CURRENT_USER", registry.CURRENT_USER,
+		"LOCAL_MACHINE", registry.LOCAL_MACHINE,
+		"USERS", registry.USERS,
+		"CURRENT_CONFIG", registry.CURRENT_CONFIG,
+		"PERFORMANCE_DATA", registry.PERFORMANCE_DATA,
+	}
+)
+
+type RegistryRetValue struct {
+	ValType        string   `json:"return_type"`
+	StringVal      string   `json:"string_val"`
+	StringArrayVal []string `json:"string_array_val"`
+	ByteArrayVal   []byte   `json:"byte_array_val"`
+	IntVal         uint32   `json:"int_val"`
+	LongVal        uint64   `json:"long_val"`
+}
+
+func lookUpKey(keyString string) (registry.Key, error) {
+	key, ok := regKeys[keyString]
+	if !ok {
+		return nil, errors.New("Registry key " + keyString + " not found")
+	}
+	return key
+}
+
+func (e *Engine) AddRegKeyString(registryString string, path string, name string, value string) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetStringValue(name, value)
+}
+
+func (e *Engine) AddRegKeyExpandedString(registryString string, path string, name string, value string) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetExpandStringValue(name, value)
+}
+
+func (e *Engine) AddRegKeyBinary(registryString string, path string, name string, value []byte) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetBinaryValu(name, value)
+}
+
+func (e *Engine) AddRegKeyDWORD(registryString string, path string, name string, value uint32) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetDWordValue(name, value)
+}
+
+func (e *Engine) AddRegKeyQWORD(registryString string, path string, name string, value uint64) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetQWordValue(name, value)
+}
+
+func (e *Engine) AddRegKeyStrings(registryString string, path string, name string, value []string) error {
+	regKey, err := lookUpKey(registryString)
+	if err != nil {
+		return err
+	}
+	openRegKey, err := registry.CreateKey(regKey, path, registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer openRegKey.Close()
+	return openRegKey.SetStringsValue(name, value)
+}
+
+func (e *Engine) DelRegKey(registryString string, path string) error {
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return err
+	}
+	return registry.DeleteKey(regKey, path)
+}
+
+func (e *Engine) DelRegKeyValue(registryString string, path string, valueName string) error {
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return err
+	}
+	regKey.DeleteValue(name)
+	return registry.DeleteKey(reg, path)
+}
+
+func (e *Engine) QueryRegKey(registryString string, path string) (RegistryRetValue, error) {
+	retVal := new(RegistryRetValue)
+	regKey, err := lookUpKey(path)
+	if err != nil {
+		return retVal, err
+	}
+	openRegKey, err := registry.OpenKey(regKey, path, registry.QUERY_VALUE)
+	if err != nil {
+		return retVal, err
+	}
+	_, valType, err := openRegKey.GetValue(path, nil)
+	if err != nil {
+		return retVal, err
+	}
+	switch valType {
+	case registry.EXPAND_SZ:
+		value, _, err := openRegKey.GetStringsValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "StringArray"
+		retVal.StringArrayVal = value
+	case registry.SZ:
+		value, _, err := openRegKey.GetStringValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "String"
+		retVal.StringVal = value
+	case registry.BINARY:
+		value, _, err := openRegKey.GetBinaryValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "ByteArray"
+		retVal.ByteArrayVal = value
+	case registry.DWORD:
+		value, _, err := openRegKey.GetIntegerValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "Uint"
+		retVal.IntVal = uint32(value)
+	case registry.QWORD:
+		value, _, err := openRegKey.GetIntegerValue(path)
+		if err != nl {
+			return retVal, err
+		}
+		retVal.ValType = "Uint64"
+		retVal.LongVal = value
+	}
+}
