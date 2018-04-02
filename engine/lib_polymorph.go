e85970c50a2cbb4789e47504874450553883a106
vyrus <vyrus001@gmail.com>
2018-04-02 14:25:34 -0700

    fixing polymorph

--- engine/lib_polymorph.go
+++ engine/lib_polymorph.go
@@ -1,76 +1,60 @@
 package engine
 
 import (
+	"debug/pe"
 	"errors"
+	"os"
 )
 
-// RetrievePolymorphicData - Retrive data stored within uninitalized space at the end of the gscript binary
-//
-// Package
-//
-// polymorph
-//
-// Author
-//
-// - Vyrus (https://github.com/vyrus001)
-//
-// Javascript
-//
-// Here is the Javascript method signature:
-//  RetrievePolymorphicData()
-//
-// Arguments
-//
-// Here is a list of the arguments for the Javascript function:
-//
-// Returns
-//
-// Here is a list of fields in the return object:
-//  * obj.data ([]byte)
-//  * obj.runtimeError (error)
-//
-// Example
-//
-// Here is an example of how to use this function in gscript:
-//  var obj = RetrievePolymorphicData();
-//  // obj.data
-//  // obj.runtimeError
-//
-func (e *Engine) RetrievePolymorphicData() ([]byte, error) {
-	return []byte{}, errors.New("not implemented for this platform")
+func getPEImgSize(peFile string) (int64, error) {
+	// open self
+	mySelf, err := pe.Open(peFile)
+	if err != nil {
+		return int64(0), err
+	}
+	defer mySelf.Close()
+
+	// get img size
+	var imgSize uint32
+	switch mySelf.Machine {
+	case 0x14c: // x86
+		imgSize = mySelf.OptionalHeader.(*pe.OptionalHeader32).SizeOfImage
+	case 0x8664: // x86_64
+		imgSize = mySelf.OptionalHeader.(*pe.OptionalHeader64).SizeOfImage
+	default:
+		return int64(0), errors.New("Binary architecture unsupported for this action.")
+	}
+	return int64(imgSize), nil
 }
 
-// WritePolymorphicData - Write data to the uninitalized space at the end of the gscript binary
-//
-// Package
-//
-// polymorph
-//
-// Author
-//
-// - Vyrus (https://github.com/vyrus001)
-//
-// Javascript
-//
-// Here is the Javascript method signature:
-//  WritePolymorphicData(data)
-//
-// Arguments
-//
-// Here is a list of the arguments for the Javascript function:
-//  * data ([]byte)
-//
-// Returns
-//
-// Here is a list of fields in the return object:
-//  * obj.runtimeError (error)
-//
-// Example
-//
-// Here is an example of how to use this function in gscript:
-//  var obj = WritePolymorphicData(data);
-//  // obj.runtimeError
-//
-func (e *Engine) WritePolymorphicData(polymorphicData []byte) error {
-	return errors.New("not implemented for this platform")
+func (e *Engine) RetrievePEPolymorphicData(peFile string) ([]byte, error) {
+	var polymorphicData []byte
+	pEImgSize, err := getPEImgSize(peFile)
+	if err != nil {
+		return polymorphicData, err
+	}
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		return polymorphicData, err
+	}
+	defer file.Close()
+	_, err = file.ReadAt(polymorphicData, pEImgSize)
+	return polymorphicData, err
+}
+
+func (e *Engine) WritePEPolymorphicData(peFile string, polymorphicData []byte) error {
+	pEImgSize, err := getPEImgSize(peFile)
+	if err != nil {
+		return err
+	}
+	file, err := os.Open(peFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteAt(polymorphicData, pEImgSize)
+	if err != nil {
+		return err
+	}
+	return nil
 }
