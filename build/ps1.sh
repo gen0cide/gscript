73ac8219cd04c1a1474d901575a1bb66a38a1398
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-04-21 12:24:12 -0600

    Dockerfile and Dockerfile-minimal fixes

--- build/ps1.sh
+++ build/ps1.sh
@@ -2,21 +2,6 @@
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 export PATH=$HOME/go/bin:$PATH
-# ------------------------------------------------------------------------------
-function parse_git_branch() {
-	git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-}
-# ------------------------------------------------------------------------------
-function get_gscript_version() {
-	local init_cwd
-	local gscript_version
-	init_cwd=$(pwd)
-	cd "$GOPATH/src/github.com/gen0cide/gscript"
-	gscript_version="$(parse_git_branch)"
-	cd "$init_cwd"
-	echo "$gscript_version"
-}
-# ------------------------------------------------------------------------------
 NO_COLOUR="\[\033[0m\]"
 RED="\[\033[00;31m\]"
 GREEN="\[\033[00;32m\]"
@@ -35,7 +20,7 @@ LPURPLE="\[\033[01;35m\]"
 LCYAN="\[\033[01;36m\]"
 LWHITE="\[\033[01;37m\]"
 WHITE="\e[97m"
-GSCRIPT_VERSION="$(get_gscript_version)"
+GSCRIPT_VERSION="master"
 # ------------------------------------------------------------------------------
-PS1="$WHITE[$RED""gscript$NO_COLOUR/$YELLOW""docker $WHITE""version$NO_COLOUR:$CYAN\$GSCRIPT_VERSION$NO_COLOUR $YELLOW\w$WHITE]$NO_COLOUR\\$ "
+PS1="$WHITE[$RED""gscript$NO_COLOUR/$YELLOW""docker $WHITE"" $YELLOW\w$WHITE]$NO_COLOUR\\$ "
 # ------------------------------------------------------------------------------
